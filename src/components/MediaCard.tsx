bfb9a666c6cce8e60f51db28c0f761edc92f7b04
Josiel <medeiros.josiel@gmail.com>
2025-05-01 15:22:57 -0300

    Add aspect radio in MediaCard

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,11 +1,12 @@
 import {
   Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
 } from "@/components/ui/card"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 interface MediaCardProps {
 
@@ -17,16 +18,18 @@ interface MediaCardProps {
 }
 export default function MediaCard({poster, title, type, year, imdbID}: MediaCardProps) {
     return (
-        <Card className="border-0 w-120">
-            <img src={poster} alt={title} className="w-full h-80 rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-2/1 lg:aspect-square" />
-            <div className="flex flex-row gap-1 opacity-50">
+        <Card className="border-0">
+            <AspectRatio ratio={16 / 9}>
+                <img src={poster} alt={title} className="w-full h-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-2/1 lg:aspect-square" />
+            </AspectRatio>
+            <div className="flex flex-row gap-2 opacity-75">
                 <p>Type: {type}</p>
                 <span>•</span>
                 <p>Year: {year}</p>
                 <span>•</span>
                 <p>IMDB ID: {imdbID}</p>
             </div>
-            <h2 className="text-lg font-bold">{title}</h2>
+            <h2 className="text-xl">{title}</h2>
         </Card>
     )
 }
