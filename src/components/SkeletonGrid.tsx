c812039f9a53d29236e0e74d2985b4e0dabd0186
Josiel <medeiros.josiel@gmail.com>
2025-05-14 13:33:10 -0300

    fix grid skeleton full width

--- src/components/SkeletonGrid.tsx
+++ src/components/SkeletonGrid.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export function SkeletonGrid() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 w-screen">
       {/* 10 skeletons */}
       {Array.from({ length: 10 }, (_, index: number) => (
         <div key={index} className="flex flex-col space-y-3">
